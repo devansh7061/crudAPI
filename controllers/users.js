9a2e6a9a60ab9fda7baad346718f2a8cb2f986e3
devansh7061 <devanshsaini67p@gmail.com>
2022-10-04 22:22:16 +0530

    added new commit

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,7 @@ export const getUsers = (req, res) => {
 };
 
 export const createUser = (req, res) => {
-  const user = req.body;
+  const user = req.body
 
   users.push({ ...user, id: uuidv4() });
   res.send(`User with name ${user.firstName} added to the database!`);
